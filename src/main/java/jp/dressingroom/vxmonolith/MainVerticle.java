cabfac0705d99ea0875b3ad898864e79effb9034
Masatoshi Itoh <masatoshi9953@gmail.com>
2024-10-15 17:54:04 +0900

    Fix: to pass test.

--- src/main/java/jp/dressingroom/vxmonolith/MainVerticle.java
+++ src/main/java/jp/dressingroom/vxmonolith/MainVerticle.java
@@ -8,7 +8,15 @@ public class MainVerticle extends AbstractVerticle {
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
 
-    vertx.deployVerticle(WebServerVerticle.class.getName());
+    vertx.deployVerticle(WebServerVerticle.class.getName()).onComplete(ar -> {
+      if (ar.succeeded()) {
+        System.out.println("WebServerVerticle started");
+        startPromise.complete();
+      } else {
+        System.out.println("WebServerVerticle failed to start");
+        startPromise.fail(ar.cause());
+      }
+    });
 
 
   }
