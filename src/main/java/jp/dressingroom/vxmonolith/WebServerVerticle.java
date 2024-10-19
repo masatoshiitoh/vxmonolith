8b872024a8cc2dd35c6f925d299f3cb107aad382
Masatoshi Itoh <masatoshi9953@gmail.com>
2024-10-20 01:12:53 +0900

    Extract handler into method.

--- src/main/java/jp/dressingroom/vxmonolith/WebServerVerticle.java
+++ src/main/java/jp/dressingroom/vxmonolith/WebServerVerticle.java
@@ -1,9 +1,11 @@
 package jp.dressingroom.vxmonolith;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.Session;
 import io.vertx.ext.web.handler.SessionHandler;
 import io.vertx.ext.web.handler.StaticHandler;
@@ -14,6 +16,7 @@ import java.util.Date;
 
 public class WebServerVerticle extends AbstractVerticle {
   public static final String TEMPLATE = ""
+    + "Path [%s]%n"
     + "Session [%s] created on %s%n"
     + "%n"
     + "Page generated on %s%n";
@@ -30,22 +33,28 @@ public class WebServerVerticle extends AbstractVerticle {
 
       // 静的コンテンツは /static 以下に配置
       // 静的コンテンツの"webroot"は、プロジェクトのトップディレクトリ内に作成する。
+      // 商用環境を想定するなら、これはCDNとかに置いてドメイン名で振り分ける部分。この書き方は、あくまで開発時の話。
       router.route("/static/*").handler(StaticHandler.create());
 
-      // トップページのルーティング設定
-      router.route("/").handler(routingContext -> {
-        Session session = routingContext.session();
-        session.computeIfAbsent("createdOn", s -> System.currentTimeMillis()); //(3)
-
-        String sessionId = session.id();
-        Date createdOn = new Date(session.<Long>get("createdOn"));
-        Date now = new Date();
-
-        routingContext.end(String.format(TEMPLATE, sessionId, createdOn, now));
-        // System.out.println("Session ID: " + sessionId);
-        // Session IDはこんな感じ Session ID: 7a83ccdb26df09b36ab673bb2f5e6a82
-      });
+      // セッションを使うルーティングハンドラを設定
+      router.route("/session/*").handler(getSessionedRoutingHandler());
+      router.route("/").handler(getSessionedRoutingHandler());
 
       server.requestHandler(router).listen(8080);
     }
+
+  private static Handler<RoutingContext> getSessionedRoutingHandler() {
+    return routingContext -> {
+      Session session = routingContext.session();
+      session.computeIfAbsent("createdOn", s -> System.currentTimeMillis()); //(3)
+
+      String sessionId = session.id();
+      Date createdOn = new Date(session.<Long>get("createdOn"));
+      Date now = new Date();
+
+      routingContext.end(String.format(TEMPLATE, routingContext.request().path(), sessionId, createdOn, now));
+      // System.out.println("Session ID: " + sessionId);
+      // Session IDはこんな感じ Session ID: 7a83ccdb26df09b36ab673bb2f5e6a82
+    };
+  }
 }
